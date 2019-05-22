af79a5eff5e254c1342218b4eadecfd6abdc1b93
wanjie <2298748148>
2019-05-22 21:35:25 +0800

    test

--- app/src/main/java/com/example/wj/wjhttp/MainActivity.java
+++ app/src/main/java/com/example/wj/wjhttp/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.wj.wjhttp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -17,11 +16,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
-        Toast.makeText(MainActivity.this,"这是Toast",Toast.LENGTH_SHORT).show();
-        Toast.makeText(MainActivity.this,"台式电脑",Toast.LENGTH_SHORT).show();
     }
 
     /**
