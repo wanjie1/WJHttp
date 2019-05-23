2e2fccbdf0cc0b721fef103559861d9524ac968a
wanjie <2298748148>
2019-05-23 17:32:08 +0800

    test

--- app/src/main/java/com/example/wj/wjhttp/http/HttpClient.java
+++ app/src/main/java/com/example/wj/wjhttp/http/HttpClient.java
@@ -1,4 +1,80 @@
 package com.example.wj.wjhttp.http;
 
+import com.example.wj.wjhttp.http.chain.Interceptor;
+import java.util.List;
+
 public class HttpClient {
+
+    private Dispatcher dispatcher;
+    private List<Interceptor> interceptors;
+    private ConnectionPool connectionPool;
+    private int retryTimes;
+    //还需要添加连接池
+
+    public Dispatcher getDispatcher() {
+        return dispatcher;
+    }
+
+    public List<Interceptor> getInterceptors() {
+        return interceptors;
+    }
+
+    public ConnectionPool getConnectionPool()
+    {
+        return connectionPool;
+    }
+
+    public int getRetryTimes() {
+        return retryTimes;
+    }
+
+    private HttpClient(Builder builder)
+    {
+        this.dispatcher = builder.dispatcher;
+        this.connectionPool = builder.connectionPool;
+        this.interceptors = builder.interceptors;
+        this.retryTimes = builder.retryTimes;
+    }
+
+    public static final class Builder
+    {
+        private Dispatcher dispatcher;
+        private List<Interceptor> interceptors;
+        private ConnectionPool connectionPool;
+        private int retryTimes;
+
+        public Builder addInterceptors(Interceptor interceptor){
+            interceptors.add(interceptor);
+            return this;
+        }
+
+        public Builder setDispather(Dispatcher dispather){
+            this.dispatcher = dispather;
+            return this;
+        }
+
+        public Builder setRetryTimes(int retryTimes){
+            this.retryTimes = retryTimes;
+            return this;
+        }
+
+        public Builder setConnectionPool(ConnectionPool connectionPool){
+            this.connectionPool = connectionPool;
+            return this;
+        }
+
+
+        public HttpClient build(){
+
+            if(null == dispatcher){
+                dispatcher = new Dispatcher();
+            }
+
+            if(null == connectionPool){
+                connectionPool = new ConnectionPool();
+            }
+            return new HttpClient(this);
+        }
+    }
+
 }
